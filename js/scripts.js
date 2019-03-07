5e001bc8d3ee083d4e496270b1c091a4012d8812
Student <example@example.com>
2019-03-06 21:08:56 -0800

    got it to show up on webpage, append, and removed bullet points from list.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
-$(document).ready(function() {
+ // user interface logic
+ $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
-    var number = parseInt($("input#number").val());
+    var n = parseInt($("#n").val());
     $("li").remove();
 
-    for (var i = 1; i <= 50; i++) {
-      if (i % 3 == 0)
-        console.log("ping");
-      else if (i % 5 == 0)
-        console.log("pong");
-      else if (i % 15 == 0)
-        console.log("pingpong");
-      else console.log(i);
+// business logic
+    for (var i = 1; i <= n ; i++) {
+      if (i % 15 == 0) {
+        $("#result").append("<li>pingpong</li>");
+      } else if (i % 3 == 0) {
+        $("#result").append("<li>ping</li>");
+      } else if (i % 5 == 0) {
+        $("#result").append("<li>pong</li>");
+      } else {
+        $("#result").append('<li> ' +  i  + ' </li>');
+      }
     }
+
     event.preventDefault();
   });
 });
